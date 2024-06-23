ed15554781eacd0094877487499c46a24e453bd8
mayumi32 <tatitahampelska@gmail.com>
2024-06-23 20:59:56 +0200

    a

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ let oldValue="stronaGlowna"
 function category(value)
 {
     if(oldValue!=null) document.getElementById(oldValue).style.display = "none";
-    let container = document.getElementById("worksContainer")
     document.getElementById(value).style.display = "block";
-    
+
+    if(window.innerWidth < 800)
+        {
+    document.getElementById('menu').style.animation="hideMenu 0.5s normal ";
+        
+        setTimeout(() => {
+            document.getElementById('menu').style.display = 'none';
+          }, 400);
+          isVisible = !isVisible
+        }
     oldValue = value;
     
 }
@@ -17,4 +25,28 @@ function showImage(imagePath)
 function hideImage()
 {
     document.getElementById('showImage').style.display = "none";
+}
+
+let isVisible = false;
+function showMenu()
+{
+    
+            isVisible= !isVisible
+            if(isVisible)
+            {
+                document.getElementById('menu').style.display = 'block'
+                document.getElementById('menu').style.animation="showMenu 0.5s normal ";
+                
+            }
+            else
+            {
+                document.getElementById('menu').style.animation="hideMenu 0.5s normal ";
+                
+                setTimeout(() => {
+                    document.getElementById('menu').style.display = 'none';
+                }, 400);
+            }
+        
+        
+    
 }
